620ad1cc1c81b3b099ac004ac15d7ee11785e20b
George Walker <george@georgewwalker.com>
2023-09-13 00:01:13 -0400

    Fix compare

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var os = require("os");
 
-if (os.platform === "win32") {
+if (os.platform == "win32") {
   module.exports = require("./win");
-} else if (os.platform === "darwin") {
+} else if (os.platform == "darwin") {
   module.exports = require("./mac");
 } else {
   throw new Error("Unsupported platform: " + os.platform);
